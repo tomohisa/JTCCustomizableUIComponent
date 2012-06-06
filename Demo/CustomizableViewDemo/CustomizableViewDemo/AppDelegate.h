94fe7066b1b2faf0c2b015fce8b97b8af0e9be5d
Tomohisa Takaoka <tomohisa.takaoka@gmail.com>
2012-06-06 12:59:29 -0700

    Initial Commit.

---
+++ Demo/CustomizableViewDemo/CustomizableViewDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomizableViewDemo
+//
+//  Created by Tomohisa Takaoka on 6/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Demo/CustomizableViewDemo/CustomizableViewDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CustomizableViewDemo
+//
+//  Created by Tomohisa Takaoka on 6/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
